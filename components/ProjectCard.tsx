d13eebc650c315dcdc15c566840201739e05e6ad
Shirshakkandel <shirshakkandel@gmail.com>
2021-07-22 13:20:24 +0545

    projectCard

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,59 +4,82 @@ import Image from "next/image";
 import { AiFillGithub, AiFillProject } from "react-icons/ai";
 // TODO Github
 import { MdClose } from "react-icons/md";
+import { fadeInUp, stagger } from "../animations";
+import { motion } from "framer-motion";
 
-const ProjectCard: FunctionComponent<{ project: IProject; showDetail: null | number; setShowDetail: (id: number | null) => void;}> = ({
+const ProjectCard: FunctionComponent<{
+  project: IProject;
+  showDetail: null | Number;
+  setShowDetail: (id: Number | null) => void;
+}> = ({
   project: {
-    id,
     name,
     image_path,
     deployed_url,
     github_url,
     description,
     key_techs,
+    id,
   },
-  showDetail,setShowDetail
+  setShowDetail,
+  showDetail,
 }) => {
-  
   // TODO scroll to top
   return (
     <>
       {/* //step 1 */}
-      <Image
+      {/* <Image
         src={image_path}
         alt={name}
         layout="responsive"
         width={300}
         height={150}
-        onClick={() => setShowDetail(id)}
+        onClick={() => setShowDetail(true)}
         className="cursor-pointer "
+      /> */}
+      <Image
+        width="300"
+        height="150"
+        src={image_path}
+        layout="responsive"
+        alt={name}
+        onClick={() => setShowDetail(id)}
+        className="cursor-pointer"
+        quality={10}
       />
-      
       {/* <img
+        width="300"
+        height="150"
         src={image_path}
         alt={name}
-        onClick={() => setShowDetail(true)}
+        onClick={() => setShowDetail(id)}
         className="cursor-pointer "
       /> */}
 
       <p className="my-2 text-center">{name}</p>
       {/* //step 1 */}
 
-      {showDetail ===id && (
-        <div className="absolute top-0 left-0 z-10 grid w-full p-2 rounded-lg bg-p-10 gray-100 md:text-black md:ph-auto md: md:p-10 dark:bg-black-100 dark:text-gray-100 md:grid-cols-2 gap-x-12 ">
-          <div>
-            <div className="border-4 border-gray-100 ">
-              {/* <Image
+      {showDetail === id && (
+        <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 rounded-lg md:p-10 dark:bg-black-100 dark:text-gray-100 md:grid-cols-2 gap-x-12 ">
+          <motion.div variants={stagger} initial="initial" animate="animate">
+            <motion.div
+              className="border-4 border-gray-100 "
+              variants={fadeInUp}
+            >
+              <Image
                 src={image_path}
                 alt={name}
                 className="overflow-hidden"
                 layout="responsive"
                 width={300}
                 height={150}
-              /> */}
-              <img src={image_path} alt={name} />
-            </div>
-            <div className="flex justify-center my-4 space-x-3">
+              />
+              {/* <img src={image_path} alt={name} /> */}
+            </motion.div>
+            <motion.div
+              className="flex justify-center my-4 space-x-3"
+              variants={fadeInUp}
+            >
               <a
                 href={github_url}
                 className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-300 rounded-sm dark:bg-black-500 "
@@ -69,13 +92,26 @@ const ProjectCard: FunctionComponent<{ project: IProject; showDetail: null | num
               >
                 <AiFillProject /> <span>Project</span>
               </a>
-            </div>
-          </div>
-          <div>
-            <h2 className="mb-3 text-xl font-medium md:text-2xl ">{name}</h2>
-            <h3 className="my-3 text-base font-medium">{description}</h3>
+            </motion.div>
+          </motion.div>
+          <motion.div variants={stagger} initial="initial" animate="animate">
+            <motion.h2
+              variants={fadeInUp}
+              className="mb-3 text-xl font-medium md:text-2xl "
+            >
+              {name}
+            </motion.h2>
+            <motion.h3
+              variants={fadeInUp}
+              className="my-3 text-base font-medium"
+            >
+              {description}
+            </motion.h3>
 
-            <div className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider">
+            <motion.div
+              variants={fadeInUp}
+              className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider"
+            >
               {key_techs.map((value, i) => (
                 <span
                   key={i}
@@ -84,8 +120,8 @@ const ProjectCard: FunctionComponent<{ project: IProject; showDetail: null | num
                   {value}
                 </span>
               ))}
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
 
           <button
             className="absolute p-1 bg-gray-200 rounded-full top-3 right-3 focus:outline-none dark:bg-black-200"
